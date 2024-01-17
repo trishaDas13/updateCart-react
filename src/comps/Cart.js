85bf6d10796718b08216bec91330235a7e51a0f2
trishaDas13 <trisha.das1308@gmail.com>
2024-01-17 16:23:58 +0530

    css added

--- src/comps/Cart.js
+++ src/comps/Cart.js
@@ -2,22 +2,35 @@ import React from 'react'
 
 const Cart = (props) => {
   return (
-    <div className='Cart'>
+    <>
+    {
+      props.carts.length === 0 ?
+      (<h3 className="empty">No product is there in your cart!</h3>):
+      (
+        <div className='cart'>
         <h1>Cart</h1>
         {
           props.carts.map((item, idx)=>{
             return(
-              <div key={idx} className="cart-item">
+              <div key={idx} className="cart_item">
                 <p>{item.name}</p>
                 <p>{item.quantity} X {item.price}</p>
               </div>
             );
           })
         }
-      <p className="total">
+      <h3 className="total"
+        style={props.carts.length === 0 ?
+          {display:'none'}: {display: 'inline'}
+        }
+      > Total: 
           ₹{props.carts.reduce((sum, ele) => sum + ele.quantity * ele.price, 0)}
-        </p>
+        </h3>
     </div>
+      )
+    }
+      
+    </>
   )
 }
 
--- src/comps/Product.jsx
+++ src/comps/Product.jsx
@@ -5,6 +5,7 @@ import Cart from "./Cart";
 //todo: reducer function
 const reducer = (state, action) => {
   switch (action.type) {
+    //todo: decrease the count of the product
     case "DECREAMENT":
       return {
         ...state,
@@ -14,17 +15,23 @@ const reducer = (state, action) => {
             : product
         ),
       };
+
+    //todo: increase the count of the product
     case "INCREAMENT":
       return {
         ...state,
         products: state.products.map((product, index) =>
           index === action.index
             ? { ...product, quantity: product.quantity + 1 }
-            : product)
+            : product
+        ),
       };
+    //todo: add the product in the cart
     case "ADDCART":
-        const selectedItem = state.products[action.index];
-      const existingItemIndex = state.carts.findIndex(item => item.name === selectedItem.name);
+      const selectedItem = state.products[action.index];
+      const existingItemIndex = state.carts.findIndex(
+        (item) => item.name === selectedItem.name
+      );
 
       if (existingItemIndex !== -1) {
         // If the product is already in the cart, update its quantity
@@ -44,27 +51,28 @@ const reducer = (state, action) => {
           carts: [...state.carts, { ...selectedItem, quantity: 1 }],
         };
       }
+    //todo: delete the product from the cart
     case "DELETECART":
-        const cartItemIndex = state.carts.findIndex(
-          item => item.name === state.products[action.index].name
+      const cartItemIndex = state.carts.findIndex(
+        (item) => item.name === state.products[action.index].name
+      );
+
+      if (cartItemIndex !== -1) {
+        // If the item is in the cart, decrease its quantity
+        const updatedCarts = state.carts.map((item, index) =>
+          index === cartItemIndex && item.quantity > 0
+            ? { ...item, quantity: item.quantity - 1 }
+            : item
         );
-  
-        if (cartItemIndex !== -1) {
-          // If the item is in the cart, decrease its quantity
-          const updatedCarts = state.carts.map((item, index) =>
-            index === cartItemIndex && item.quantity > 0
-              ? { ...item, quantity: item.quantity - 1 }
-              : item
-          );
-  
-          return {
-            ...state,
-            carts: updatedCarts.filter(item => item.quantity > 0),
-          };
-        } else {
-          // Item not in the cart, do nothing
-          return state;
-        }
+
+        return {
+          ...state,
+          carts: updatedCarts.filter((item) => item.quantity > 0),
+        };
+      } else {
+        // Item not in the cart, do nothing
+        return state;
+      }
     default:
       return state;
   }
@@ -73,11 +81,11 @@ const reducer = (state, action) => {
 const Product = () => {
   //todo: declair reducer
   let initalState = {
-    products:[...Data],
-    carts:[]
-  };  
+    products: [...Data],
+    carts: [],
+  };
   const [state, dispatch] = useReducer(reducer, initalState);
- 
+
   //todo: render element
   return (
     <>
@@ -92,20 +100,18 @@ const Product = () => {
                 <div className="counter">
                   <button
                     onClick={() => {
-                        dispatch({ type: "DECREAMENT", index: idx })
-                        dispatch({type: "DELETECART", index: idx})
-                      }
-                    }
+                      dispatch({ type: "DECREAMENT", index: idx });
+                      dispatch({ type: "DELETECART", index: idx });
+                    }}
                   >
                     -
                   </button>
                   <span>{item.quantity}</span>
                   <button
                     onClick={() => {
-                        dispatch({type: "INCREAMENT", index: idx })
-                        dispatch({type: "ADDCART", index: idx})
-                      }
-                    }
+                      dispatch({ type: "INCREAMENT", index: idx });
+                      dispatch({ type: "ADDCART", index: idx });
+                    }}
                   >
                     +
                   </button>
@@ -115,7 +121,7 @@ const Product = () => {
           })}
         </div>
       </div>
-      <Cart carts= {state.carts}/>
+      <Cart carts={state.carts} />
     </>
   );
 };
